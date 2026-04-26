013e956c1d1b5326ea7c7b6ef29a4f0c4433ad81
boikovanton <boikovanton1987@gmail.com>
2026-04-26 15:35:41 -0700

    Add fullName getter and fix nodemon script

--- models/customer.js
+++ models/customer.js
@@ -14,6 +14,12 @@ class Customer {
     this.notes = notes;
   }
 
+  /** get full name */
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** find all customers. */
 
   static async all() {
